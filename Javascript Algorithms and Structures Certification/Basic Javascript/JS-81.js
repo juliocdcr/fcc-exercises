dff3f737a440705c0f4076c386fde44e65f25b7e
Julio Damigo <julio.cdcr@gmail.com>
2021-02-20 01:05:11 -0500

    JS 81 - Build JavaScript Objects

---
+++ Javascript Algorithms and Structures Certification/Basic Javascript/JS-81.js
@@ -0,0 +1,8 @@
+var myDog = {
+    // Only change code below this line
+    "name": "Tom",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["man", "duck"]
+    // Only change code above this line
+};
