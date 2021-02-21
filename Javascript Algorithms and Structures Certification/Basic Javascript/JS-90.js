4c65baa805ae148b573abd42a5b9260848ccad51
Julio Damigo <julio.cdcr@gmail.com>
2021-02-21 05:31:15 -0500

    JS 90 - Manipulating Complex Objects

---
+++ Javascript Algorithms and Structures Certification/Basic Javascript/JS-90.js
@@ -0,0 +1,23 @@
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Drake",
+        "title": "Scorpion",
+        "release_year": 2010,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ]// Add a record here
+    }
+];
