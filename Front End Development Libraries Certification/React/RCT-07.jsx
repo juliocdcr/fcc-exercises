88f14a3ee7da023f29b88a39ec0fefe2b2bfcef6
Julio Damigo <julio.cdcr@gmail.com>
2021-12-25 15:15:29 -0500

    RCT 07 - Create a Stateless Functional Component

---
+++ Front End Development Libraries Certification/React/RCT-07.jsx
@@ -0,0 +1,11 @@
+const MyComponent = function () {
+    // Change code below this line
+    return (
+        <div>
+            <p>Stateless Function Component</p>
+        </div>
+    );
+
+
+    // Change code above this line
+}
